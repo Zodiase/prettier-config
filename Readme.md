82cfe2b7e92fe6073dd157f9ab20574e7c547509
Xingchen Hong <zodiase@gmail.com>
2021-06-27 16:30:33 -0700

    Initial files.

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# `@xch/prettier-config`
+
+> My personal [Prettier](https://prettier.io) config
+
+## Usage
+
+**Install**:
+
+```bash
+$ npm install --dev @xch/prettier-config
+```
+
+**Edit `package.json`**:
+
+```jsonc
+{
+    // ...
+    "prettier": "@xch/prettier-config"
+}
+```
+
+---
+
+This package was created following this guide here: https://prettier.io/docs/en/configuration.html#sharing-configurations
---
+++ prettier.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // Set to 120 to accommodate most of high-resolution setups.
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'always',
+    proseWrap: 'preserve',
+    endOfLine: 'lf',
+    embeddedLanguageFormatting: 'off',
+};
